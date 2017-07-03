5debf6eca3b593f12cdbb2af58a8c5a3ee26edb5
ecly <elyne@live.dk>
2017-07-03 12:54:12 +0200

    finished syntax in functions section

--- haskell-ghci-playground/syntax-in-functions.hs
+++ haskell-ghci-playground/syntax-in-functions.hs
@@ -25,3 +25,68 @@ head' (x:_) = x
 firstLetterOfWord all@(x:xs) = "First letter of " ++ all ++ " is " ++ [x]
 
 -- guards
+bmiTell weight height  
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise                 = "You're a whale, congratulations!"   
+
+-- inline guards... don't use these
+max' a b | a > b = a | otherwise = b  
+
+-- 'infix' function definition
+a `eq` b 
+    | a == b = True
+    | otherwise = False
+
+-- where -> eg. avoid repetition
+bmiTell' weight height  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+
+-- multiple where expressions
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname 
+
+-- let bindings => let <bindings> in <expression>
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea  
+
+-- let bindings can be used almost anywhere unlike where eg.
+res' = 4 * (let a = 9 in a + 1) + 2  
+res'' = [let square x = x * x in (square 5, square 3, square 2)]  
+
+-- multiple inline let binds -> separate with semicolon
+res1 = (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)  
+
+-- list comprehenseion with let binds
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]  
+
+-- case expressions
+head' :: [a] -> a  
+head'' xs = case xs of [] -> error "No head for empty lists!"  
+                       (x:_) -> x  
+
+{- case expression of pattern -> result  
+                    pattern -> result  
+                    pattern -> result  
+                    ...  
+-}
+
+-- where / case comparison example
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list."  
+
+describeList' :: [a] -> String  
+describeList' xs = "The list is " ++ what xs  
+    where what [] = "empty."  
+          what [x] = "a singleton list."  
+          what xs = "a longer list."  
